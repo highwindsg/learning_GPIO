09ef9f4351ca2a3864504e6ad414196d4408a9c3
Caven Yip <highwind.sg@gmail.com>
2018-09-29 20:19:06 +0800

    new file
    new file

--- LEDbutton.py
+++ LEDbutton.py
@@ -14,15 +14,17 @@ buttonPin = 5   # GPIO5 is where one of the button pin is connected to.
 # Setup what the GPIO22 is.
 GPIO.setup(LEDPin, GPIO.OUT)    # So setup LEDPin is an output.
 # Setup the button.
+# As this button is a up and down button, it is currently in a up position.
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-
+# To keep track of what is the state of the button,
+# which is the up state, which is True as in it is open right now.
 buttonPress = True
-ledState = False
+ledState = False    # The LED is off right now, so is False.
 
 try:
     while count < blinkCount:       # Which is True
         print("Come on man, press the button!")
-        buttonPress = GPIO.input(buttonPin)
+        buttonPress = GPIO.input(buttonPin) # GPIO get the buttonPin input.
         if buttonPress == False and ledState == False:
             GPIO.output(LEDPin, True)
             print("LED ON")
@@ -36,5 +38,5 @@ try:
             sleep(0.5)
         sleep(0.1)
 finally:
-    GPIO.output(LEDPin, False)
+    GPIO.output(LEDPin, False)      # To off everything safely just in case.
     GPIO.cleanup()                  # Reset the GPIO Pins to a safe state.`
