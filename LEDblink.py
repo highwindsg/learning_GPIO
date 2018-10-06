e4447beb6a42222f01a73191cd343d50a70a86f3
Caven Yip <highwind.sg@gmail.com>
2018-10-06 12:53:38 +0800

    new file
    new file

--- LEDblink.py
+++ LEDblink.py
@@ -11,16 +11,16 @@ count = 0
 LEDPin = 22     # GPIO22 is where the LED's longer +ve pin is connected to.
 
 # Setup what the GPIO22 is.
-GPIO.setup(LEDPin, GPIO.OUT)    # So setup LEDPin is an output.
+GPIO.setup(LEDPin, GPIO.OUT)    # So setup LEDPin as an output.
 
 try:
     while count < blinkCount:       # Which is True
         GPIO.output(LEDPin, True)   # LED blinks On
         print("LED ON")
-        sleep(3)                    # Wait for 3 secs
+        sleep(3)                    # Wait for 3 secs with LED ON
         GPIO.output(LEDPin, False)  # LED is Off
         print("LED OFF")
         sleep(1)                    # Wait for 1 sec
-        count += 1                  # Loop until 3 blink count to stop.
+        count += 1                  # while loop continue for another 2 round.
 finally:
-    GPIO.cleanup()                  # Reset the GPIO Pins to a safe state.`
+    GPIO.cleanup()                  # Reset the GPIO Pins to a safe state.
